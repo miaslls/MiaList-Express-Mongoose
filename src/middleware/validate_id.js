9cb7dea8d05b4ab3e80fad1bf8079c931c987262
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-17 18:51:16 -0300

    feat(middleware): validate_id

---
+++ src/middleware/validate_id.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export default validate_id = (req, res, next) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: 'ID INVALID' });
+  }
+
+  next();
+};
--- src/users/user.route.js
+++ src/users/user.route.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 import * as controller from './user.controller.js';
+import validate_id from '../middleware/validate_id.js';
 
 router.post('/', controller.createUser);
 router.get('/', controller.findAllUsers);
-router.patch('/:id', controller.updateUser);
-router.delete('/:id', controller.removeUser);
+router.patch('/:id', validate_id, controller.updateUser);
+router.delete('/:id', validate_id, controller.removeUser);
 
 export default router;
