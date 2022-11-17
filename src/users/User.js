77ea85632699943d4ef2d186f3f8a51f3a6314b8
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-17 14:28:19 -0300

    feat: User.js

---
+++ src/users/User.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, select: false },
+    isAdmin: { type: Boolean, required: true },
+  },
+  { versionKey: false },
+);
+
+UserSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', UserSchema, 'users');
+
+export default User;
