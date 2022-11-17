8a72a562b6b6e1cd5946cfb884046be6b2bea801
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-17 14:21:47 -0300

    feat: databaseConnection

---
+++ src/database/dbConnection.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+const databaseConnection = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('mongodb connected ❤'))
+    .catch((err) => console.log(`error connecting to database: ${err}`));
+};
+
+export default databaseConnection;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+import databaseConnection from './database/dbConnection.js';
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+databaseConnection();
 
 app.use(cors());
 app.use(express.json());
