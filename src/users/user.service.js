2e8f1cba2d68423ff99e027690d3bf8317d0fa49
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-18 12:49:45 -0300

    feat(user): sort users

--- src/users/user.service.js
+++ src/users/user.service.js
@@ -2,7 +2,7 @@ import User from './User.js';
 
 export const create = (body) => User.create(body);
 
-export const findAll = () => User.find();
+export const findAll = () => User.find().sort({ username: 'asc' });
 
 export const findById = (id) => User.findById(id);
 
