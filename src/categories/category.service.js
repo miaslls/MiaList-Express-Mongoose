b0195ac72eedc3f43301ed7f078256fc46682c7e
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-18 12:50:09 -0300

    feat(categ): service

--- src/categories/category.service.js
+++ src/categories/category.service.js
@@ -2,4 +2,12 @@ import Category from './Category.js';
 
 export const create = (body) => Category.create(body);
 
-export const findAllByUser = (userId) => Category.find({ user: userId }).sort({ ['name']: 1 });
+export const findAllByUser = (userId) => Category.find({ user: userId }).sort({ name: 'asc' });
+
+export const findById = (id) => Category.findById(id);
+
+export const findByName = (name) => Category.findOne({ name: name });
+
+export const update = (id, body) => Category.findByIdAndUpdate(id, body).setOptions({ returnOriginal: false });
+
+export const remove = (id) => Category.findByIdAndRemove(id).select('-password');
