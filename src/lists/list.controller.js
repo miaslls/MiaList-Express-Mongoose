51a449b50580614f96b4163ab0d0cb7947160825
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-19 17:00:38 -0300

    feat(list): update

--- src/lists/list.controller.js
+++ src/lists/list.controller.js
@@ -1,11 +1,5 @@
 import * as service from './list.service.js';
 
-// export const FUNCT = async (req, res) => {
-//     try {} catch (err) {
-//         res.status(500).send({ message: err.message });
-//       }
-// }
-
 // 📌 POST
 
 export const createList = async (req, res) => {
@@ -15,6 +9,8 @@ export const createList = async (req, res) => {
 
     const listByTitle = await service.findByTitle(reqBody.title, loggedUser._id);
     if (listByTitle) return res.status(400).send({ message: 'LIST NOT UNIQUE' });
+
+    // TODO: add to category
 
     const body = { ...reqBody, user: loggedUser._id };
     const list = await service.create(body);
@@ -33,6 +29,39 @@ export const findAllListsByUser = async (req, res) => {
     const lists = await service.findAllByUser(loggedUser._id);
 
     res.send(lists);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+// 📌 PATCH
+
+export const updateList = async (req, res) => {
+  try {
+    const loggedUser = req.user;
+    const listId = req.params.id;
+    const body = req.body;
+
+    const listToUpdate = await service.findById(listId);
+    if (!listToUpdate) return res.status(404).send({ message: 'NOT FOUND' });
+
+    const listUserId = listToUpdate.user.toString();
+    if (listUserId !== loggedUser._id) return res.status(403).send({ message: 'FORBIDDEN' });
+
+    const listByTitle = await service.findByTitle(body.title, loggedUser._id);
+
+    console.log(listByTitle?.title, body.title, listUserId, loggedUser._id); // 🐞
+
+    if (listByTitle) {
+      console.log(listToUpdate.title !== body.title); // 🐞
+      if (listToUpdate.title !== body.title) return res.status(400).send({ message: 'CATEGORY NOT UNIQUE' });
+    }
+
+    // TODO: if ('category' in body) add/remove from category
+
+    const list = await service.update(listId, body);
+
+    res.send(list);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
--- src/lists/list.route.js
+++ src/lists/list.route.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 import * as controller from './list.controller.js';
 
+import validate_id from '../middleware/validate_id.js';
+
 router.post('/', controller.createList);
 router.get('/', controller.findAllListsByUser);
+router.patch('/:id', validate_id, controller.updateList);
 
 export default router;
