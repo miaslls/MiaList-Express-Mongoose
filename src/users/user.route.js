2e4b70268cdee1faacfa8161d51660bb458cdf68
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-22 05:43:54 -0300

    OCD: destructure

--- src/users/user.route.js
+++ src/users/user.route.js
@@ -3,11 +3,11 @@ const controller = require('./user.controller');
 
 const authorize = require('../middleware/authorize');
 const validate_id = require('../middleware/validate_id');
-const middleware = require('./user.middleware');
+const { validateBody_post, validateBody_patch } = require('./user.middleware');
 
-user.post('/', middleware.validateBody_post, controller.createUser);
+user.post('/', validateBody_post, controller.createUser);
 user.get('/', authorize, controller.findAllUsers);
-user.patch('/:id', authorize, validate_id, middleware.validateBody_patch, controller.updateUser);
+user.patch('/:id', authorize, validate_id, validateBody_patch, controller.updateUser);
 user.delete('/:id', authorize, validate_id, controller.removeUser);
 
 module.exports = user;
