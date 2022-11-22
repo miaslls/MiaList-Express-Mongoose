503d7c7415fa1e420ec2d1807dddd80c68a393a4
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-21 21:38:45 -0300

    refactor: User

--- src/users/User.js
+++ src/users/User.js
@@ -6,6 +6,10 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, select: false },
     isAdmin: { type: Boolean, required: true, default: false },
+    profiles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }],
+    name: { type: String },
+    email: { type: String },
+    CPF: { type: Number },
   },
   { versionKey: false },
 );
