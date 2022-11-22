92a98db75a7b413d2d3afdc4217ff5413c07e964
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-22 02:56:17 -0300

    chore: remove unused

--- src/index.js
+++ src/index.js
@@ -10,10 +10,6 @@ const authRoute = require('./auth/auth.route');
 const userRoute = require('./users/user.route');
 const profileRoute = require('./profiles/profile.route');
 
-// import tagRoute from './tags/tag.route.js';
-// import listRoute from './lists/list.route.js';
-// import entryRoute from './entries/entry.route.js';
-
 const authorize = require('./middleware/authorize');
 
 const app = express();
@@ -27,9 +23,6 @@ app.use(express.json());
 app.use('/login', authRoute);
 app.use('/user', userRoute);
 app.use('/profile', authorize, profileRoute);
-// app.use('/tag', authorize, tagRoute);
-// app.use('/list', authorize, listRoute);
-// app.use('/entry', authorize, entryRoute);
 
 app.listen(port, () => {
   console.log(`server running @ port ${port}`);
