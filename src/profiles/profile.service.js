812d95896f5b568e3ac3e9ca8c3baaa35ab78a7f
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-22 03:48:40 -0300

    fix: minor

--- src/profiles/profile.service.js
+++ src/profiles/profile.service.js
@@ -2,8 +2,7 @@ const Profile = require('./Profile');
 
 const create = (body) => Profile.create(body);
 
-const findAllByUser = (userId) =>
-  Profile.find({ user: userId }).populate('user').populate('tags').populate('lists').sort({ name: 'asc' });
+const findAllByUser = (userId) => Profile.find({ user: userId }).populate('user').sort({ name: 'asc' });
 
 const findById = (id) => Profile.findById(id).populate('user').populate('tags').populate('lists');
 
